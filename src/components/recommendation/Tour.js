e3d197b67d60b49e41da1f52029c9d614def0c3c
yueunbi <rachelyu.1025@gmail.com>
2022-09-18 12:44:38 +0900

    ✨ feat : 앨범형 캐러셀 레이아웃 구현
    - 앨범형 캐러셀 레이아웃 구현
    - 기능 구현 후 조금 더 수정예정
    
    issues : #5

--- src/components/recommendation/Tour.js
+++ src/components/recommendation/Tour.js
@@ -1,11 +1,12 @@
 import styled, { ThemeProvider } from 'styled-components';
-import arrow from '../../assets/icon/ico_route_goods.png'
-import pause from '../../assets/icon/bg_pause2.png'
-import play from '../../assets/icon/bg_play2.png'
-import listIcon from '../../assets/icon/ico_sort_list.png'
-import albumIcon from '../../assets/icon/ico_sort_album_on.png'
-import prevBtn from '../../assets/icon/btn_goods_prev.png'
-import nextBtn from '../../assets/icon/btn_goods_next.png'
+import arrow from 'assets/icon/ico_route_goods.png'
+import albumArrow from 'assets/icon/arr_location.png'
+import pause from 'assets/icon/bg_pause2.png'
+import play from 'assets/icon/bg_play2.png'
+import listIcon from 'assets/icon/ico_sort_list.png'
+import albumIcon from 'assets/icon/ico_sort_album_on.png'
+import prevBtn from 'assets/icon/btn_goods_prev.png'
+import nextBtn from 'assets/icon/btn_goods_next.png'
 
 // import Bold from '../../assets/fonts/NanumGothicBold.woff'
 
@@ -31,24 +32,26 @@ const IconContainer = styled.div`
 `
 
 const ContentContainer = styled.div`
-  width: 1220px;
+  width: 100vw;
+  /* width: 1220px; */
   height : 480px;
   /* border : 1px solid blue; */
-  margin: 10px 100px;
+  margin: 10px;
   padding: 0;
 `
 
 const FlightsContainer = styled.ul`
   display: flex;
   flex-direction: column;
+  justify-content: center;
   flex-wrap: wrap;
-  width : 100%;
+  /* width : 100%; */
   /* height: 100%; */
-  /* width: 1220px; */
+  width: 1220px;
   height : 450px;
   margin: 0;
-  padding: 0; 
-  /* overflow: hidden; */
+  padding-left: 6%;
+  overflow: hidden;
 `;
 
 const FlightLi = styled.li`
@@ -59,6 +62,7 @@ const FlightLi = styled.li`
   border-radius: 5px;
   margin: 0 10px 10px;
   width: 590px;
+  /* width: 40%; */
   height : 75px;
   background-color: ${props => props.theme.gray};
   color : #fff;
@@ -67,7 +71,31 @@ const FlightLi = styled.li`
 `
 
 const FlightAlbum = styled.li`
+  display: flex;
+  flex-direction: column;
+  list-style: none;
+  margin: 0 15px;
+  padding: 0;
+  width :  282px;
+  /* border : 1px solid blue; */
+  background-color: ${props => props.theme.white};
+  height: 380px;
+`
 
+const AlbumAbout = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 30px 10px;
+  height: 197px;
+`
+const AlbumTrip = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-around;
+  align-items: center;
+  width: 80%;
+  margin: 10px;
 `
 
 const PriceContainer = styled.div`
@@ -76,7 +104,15 @@ const PriceContainer = styled.div`
   align-items: center;
 `
 
+const AlbumPriceContainer = styled.div`
+  display: flex;
+  justify-content: space-around;
+  width: 40%;
+`
+
 const Paging = styled.div`
+  position: relative;
+  right: 1.5%;
   display: flex;
   justify-content: center;
   align-items: center;
@@ -90,7 +126,7 @@ border-radius: 10px;
 margin: 5px;
 `
 
-const Tour = ({flights}) => {
+export const Tour = ({flights}) => {
   const devision = (<span style={{fontWeight: 300}}>|</span>)
 
   const flightLi = flights.map(list => (
@@ -117,12 +153,37 @@ const Tour = ({flights}) => {
     </FlightLi>
     ));
 
+  const flightAlbum = flights.map(list => (
+  <FlightAlbum key={list.id}>
+    <img src={list.pic}/>
+
+    <AlbumAbout>
+      <span style={{backgroundColor: 'lightgray', borderRadius: '50px', padding: '5px 10px' }}>{list.about}</span>
+
+      <AlbumTrip style={{ height: '100%'}}>
+        <span>{list.departure}</span>
+        <span> 
+          <img src={albumArrow} />
+        </span> 
+        <span>{list.destination}</span>
+      </AlbumTrip>
+
+      <AlbumPriceContainer>
+        <span style={{fontSize: "5px"}}>KRW</span>
+        <span>{list.price} ~</span>
+      </AlbumPriceContainer>
+
+    </AlbumAbout>
+
+  </FlightAlbum>
+  ));
+
   return (
     <>
     <CarouselContainer style={{position: 'relative'}}>
       <CarouselHeader>
-        <h2 style={{position: 'relative', left: "30px"}}>최저가 여행</h2>
-        <div style={{position: 'relative', left: '490px'}}>
+        <h2>최저가 여행</h2>
+        <div style={{position: 'absolute', right: "11%"}}>
           <IconContainer>
             <img src={listIcon} style={{position: 'relative', top: '50%', transform: 'translateY(-50%)'}} />
           </IconContainer>
@@ -134,8 +195,10 @@ const Tour = ({flights}) => {
 
       <ContentContainer>
         <FlightsContainer>
-          {flightLi}
+          {/* {flightLi} */}
+          {flightAlbum}
         </FlightsContainer>
+        
         <Paging>
           <Indicator style={{backgroundColor: "#808080" }}/>
           <Indicator/>
@@ -143,20 +206,24 @@ const Tour = ({flights}) => {
             <img src={pause}/>
           </div>
         </Paging>
-
       </ContentContainer>
 
       <div style={{position:'absolute', top: '50%', transform: 'translateY(-50%)', left: '3%'}}>
         <img src={prevBtn}/>
       </div>
+
       <div style={{position:'absolute', top: '50%', transform: 'translateY(-50%)', right: '3%'}}>
         <img src={nextBtn} />
       </div>
     </CarouselContainer>
 
+
+      {/* <div>
+      {flightAlbum}
+      </div> */}
     </>
   )
 }
 
-export default Tour;
+
 
