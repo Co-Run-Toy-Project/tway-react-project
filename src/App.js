ee7e7543c5eac42351d01db66cb1888d862acc73
dorrion <dhdms8696@naver.com>
2022-09-20 22:27:09 +0900

    🔨 refactor: App.js로 병합

--- src/App.js
+++ src/App.js
@@ -4,14 +4,23 @@ import theme from "data/theme";
 
 import './App.css';
 import { Event } from './components/event/Event';
-import flights from 'data/flights';
 import { Tour } from 'components/recommendation/Tour';
+import { HeaderNav } from './components/navigation/components/HeaderNav';
+import Reservation from './components/flight-check/Reservation'
+import AffIcon from './components/aliance/AffIcon';
+
+
+import flights from './data/flights'
 
 function App() {
   return (
     <>
       <ThemeProvider theme = {theme}>
+        <HeaderNav />
+        <Reservation />
+        <AffIcon />
         <Event/>
+        <Tour flights={flights}/>
       </ThemeProvider>
     </>
   );
