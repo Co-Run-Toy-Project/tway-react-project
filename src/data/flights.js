a543b6a4d93a981ee46c0ac49f28af4941b81fad
EUNBI <71588080+rachelyu1025@users.noreply.github.com>
2022-09-20 22:03:47 +0900

    Feature/issue#5 최저가 여행 Carousel layout 구현완료 (#21)
    * ✨ feat : 최저가 여행 캐러셀 list형 레이아웃 구현
    
    - header
    - content
    - left & right buttons
    - indicator
    
    Issues: #5
    
    * ✨ feat : 색상추가
    
    - theme.js 내 색상코드 추가
    
    issue : #5
    
    * ✨ feat : 최저가 여행 dummy data 추가
    
    - 최저가 여행 캐러셀 관련 dummy data 파일 생성
    
    issue : #5
    
    * ✨ feat : 앨범형 캐러셀 레이아웃 구현
    
    - 앨범형 캐러셀 레이아웃 구현
    - 기능 구현 후 조금 더 수정예정
    
    issues : #5
    
    Co-authored-by: Oheun Kim <dhdms8696@naver.com>

---
+++ src/data/flights.js
@@ -0,0 +1,127 @@
+const flights = [
+  {
+    id: 0,
+    date: '09.23',
+    departure: '인천',
+    destination: '올란바타르',
+    price: '172,600',
+    about: '편도 총액',
+    pic: '../assets/images/7ac7dc5d-3311-4409-92b9-663b23289d93.jpg',
+  },
+  {
+    id: 1,
+    date: '09.12',
+    departure: '인천',
+    destination: '나리타',
+    price: '102,000',
+    about: '편도 총액',
+    pic: '../assets/images/7603458d-4ab4-4a7e-ab7d-510baecd8998.jpg',
+  },
+  {
+    id: 2,
+    date: '09.12',
+    departure: '인천',
+    destination: '방콕/수완나폼',
+    price: '150,460',
+    about: '편도 총액',
+    pic: '../assets/images/0ad33585-8f1a-410f-a5f6-fd3a08cc6f29.jpg',
+  },
+  {
+    id: 3,
+    date: '09.24',
+    departure: '대구',
+    destination: '다낭',
+    price: '123,600',
+    about: '편도 총액',
+    pic: '../assets/images/7b9e4206-db0d-4213-875f-58161e71fd62.jpg',
+
+  },
+  {
+    id: 4,
+    date: '09.13',
+    departure: '김포',
+    destination: '제주',
+    price: '27,100',
+    about: '편도 총액',
+    pic: '../assets/images/0908a425-b0c7-4292-965a-44e2a081502e.jpg',
+
+  },
+  {
+    id: 5,
+    date: '09.12',
+    departure: '인천',
+    destination: '싱가포르',
+    price: '154,000',
+    about: '편도 총액',
+    pic: '../assets/images/118237ee-f460-4f9b-9dd0-1e9988079080.png',
+
+  },
+  {
+    id: 6,
+    date: '09.15',
+    departure: '인천',
+    destination: '다낭',
+    price: '128,600',
+    about: '편도 총액',
+    pic: '../assets/images/7b9e4206-db0d-4213-875f-58161e71fd62.jpg',
+
+  },
+  {
+    id: 7,
+    date: '09.12',
+    departure: '인천',
+    destination: '괌',
+    price: '149,350',
+    about: '편도 총액',
+    pic: '../assets/images/2f36ff33-21c7-46ed-821a-a1abf6f11505.jpg',
+
+  },
+  {
+    id: 8,
+    date: '09.12',
+    departure: '인천',
+    destination: '후쿠오카',
+    price: '78,300',
+    about: '편도 총액',
+    pic: '../assets/images/e5d99ad3-1171-43b5-a175-fd80f3ea9234.jpg',
+  },
+  {
+    id: 9,
+    date: '09.12',
+    departure: '김포',
+    destination: '부산',
+    price: '29,500',
+    about: '편도 총액',
+    pic: '../assets/images/1571374d-6fcd-4130-810d-e707bd6683fa.jpg',
+
+  },
+  {
+    id: 10,
+    date: '09.14',
+    departure: '대구',
+    destination: '제주',
+    price: '30,500',
+    about: '편도 총액',
+    pic: '../assets/images/0908a425-b0c7-4292-965a-44e2a081502e.jpg',
+  },
+  {
+    id: 11,
+    date: '09.20',
+    departure: '광주',
+    destination: '제주',
+    price: '32,500',
+    about: '편도 총액',
+    pic: '../assets/images/0908a425-b0c7-4292-965a-44e2a081502e.jpg',
+  },
+  {
+    id: 12,
+    date: '10.04',
+    departure: '광주',
+    destination: '양양',
+    price: '73,500',
+    about: '편도 총액',
+    pic: '../assets/images/ca02ff6d-c4de-4f21-8973-1535edc7f69c.jpg',
+  }
+]
+
+export default flights;
